765543eeb7e5d4c152bf7bd4bf6560ac6bc2a9bb
Pawel Pesz <pawel.pesz@telushealth.com>
2025-04-24 16:51:23 +0100

    feat: Module use example for Terraform 1.11+

---
+++ examples/simple-terraform-1-11/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">= 0.15"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "aws" {
+  alias  = "replica"
+  region = var.replica_region
+}
+
+module "remote_state" {
+  source = "../../"
+
+  # Do not create the DynamoDB lock table
+  create_dynamodb_table = false
+  # Instead, add S3 lock file permissions to the IAM policy
+  terraform_iam_policy_add_lockfile_permissions = true
+
+  providers = {
+    aws         = aws
+    aws.replica = aws.replica
+  }
+}
+
+resource "aws_iam_user" "terraform" {
+  name = "TerraformUser"
+}
+
+resource "aws_iam_user_policy_attachment" "remote_state_access" {
+  user       = aws_iam_user.terraform.name
+  policy_arn = module.remote_state.terraform_iam_policy.arn
+}
